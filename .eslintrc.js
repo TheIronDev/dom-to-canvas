7035f226b3fdcf5243c927dd0ceefb2215688523
Tyler Stark <theirondeveloper@gmail.com>
2016-05-13 21:31:54 -0700

    Adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "extends": "google",
+    "env": {
+        "browser": true
+    }
+};
--- dom-to-canvas.js
+++ dom-to-canvas.js
@@ -10,7 +10,7 @@
  *  Convert a fetched document into an abstract DOM-like structure.
  *  Render a visual representation of the DOM-like structure.
  */
-
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "domToCanvas" }] */
 'use strict';
 
 /**
@@ -18,7 +18,6 @@
  * renderCurrentDOM(), rendering a canvas onto the current document.
  */
 var domToCanvas = (function(domToCanvasOpts) {
-
   /**
    * The document host object maintains a live HTMLCollection of certain element tags.
    *
@@ -35,7 +34,7 @@ var domToCanvas = (function(domToCanvasOpts) {
     BODY: function(newNode, node, documentRef) {
       documentRef.body = newNode;
     },
-    FORM: function (newNode, node, documentRef) {
+    FORM: function(newNode, node, documentRef) {
       documentRef.forms.push(newNode);
     },
     SCRIPT: function(newNode, node, documentRef) {
@@ -83,34 +82,33 @@ var domToCanvas = (function(domToCanvasOpts) {
    * drawNodes() gets called for every node in the dom-tree. That means for every node we reinstantiate two
    * variables that don't ever change.
    */
-  var startAngle = 0,
-      endAngle = 2 * Math.PI,
-      radius = 5;
+  var startAngle = 0;
+  var endAngle = 2 * Math.PI;
+  var radius = 5;
 
   // I'm scoping these variables to the top so they can be used in multiple functions without having to pass them around.
   var cellHeight;
   var currentTree;
   var currentHoveredNode;
-  var currentHoveredNodeBackgroundColor;
+  var previousNodeBackgroundColor;
   var currentTreeHeight;
   var ctx;
   var treeStack = [];
 
   /**
    * Traverse down an document, creating a DOM-like structure
-   * @param node - the true DOM node
-   * @param parentNode - a dom-like representation of our DOM node
-   * @param depth - the node's depth, used for the rendering process
-   * @param start - the starting range to draw on the canvas
-   * @param end - the ending range to draw on the canvas
-   * @param documentRef {Object} - An object literal that will get merged into our document-like element after traversal
+   * @param {Element} node - the DOM node
+   * @param {Element} parentNode - a dom-like representation of our DOM node
+   * @param {Number} depth - the node's depth, used for the rendering process
+   * @param {Number} start - the starting range to draw on the canvas
+   * @param {Number} end - the ending range to draw on the canvas
+   * @param {Object} docParams - An object literal that will get merged into our document-like element after traversal
    *
-   * @returns {Object} DOM-like object.
+   * @return {Object} DOM-like object.
    */
-  function traverseDomLikeNode(node, parentNode, depth, start, end, documentRef) {
-
-    if (depth > documentRef.largestDepth) {
-      documentRef.largestDepth = depth;
+  function traverseDomNodes(node, parentNode, depth, start, end, docParams) {
+    if (depth > docParams.largestDepth) {
+      docParams.largestDepth = depth;
     }
 
     /**
@@ -154,10 +152,10 @@ var domToCanvas = (function(domToCanvasOpts) {
       __nodeRef: node.__nodeRef || node // create a reference to the original node. This does NOT exist on the DOM Element
     };
 
-
-    var attributes = node.attributes,
-        attributesLength,
-        attribute;
+    var attributes = node.attributes;
+    var attributesLength;
+    var attribute;
+    var i;
 
     /**
      * In the event we are traversing DOM-like nodes (and not real DOM nodes), we can simply set the
@@ -168,10 +166,8 @@ var domToCanvas = (function(domToCanvasOpts) {
      * (real) Element.attributes returns a NamedNodeMap
      */
     if (attributes instanceof NamedNodeMap) {
-
       attributesLength = attributes.length || 0;
-      for(var i = 0; i < attributesLength; i++) {
-
+      for (i = 0; i < attributesLength; i++) {
         /**
          * If you know that attribute you are setting or getting, you almost always will want to use
          * node.getAttribute(str) or node.setAttribute(). The getter returns the string value of the
@@ -188,43 +184,43 @@ var domToCanvas = (function(domToCanvasOpts) {
     }
 
     // If the node has an id, then lets add it directly to the docRef ID map.
-    if(node.id) {
+    if (node.id) {
       newNode.id = node.id;
-      documentRef.ids[node.id] = newNode;
+      docParams.ids[node.id] = newNode;
     }
 
-    // If our node is among a set of special cases, lets call a function that updates the documentRef
+    // If our node is among a set of special cases, lets call a function that updates the docParams
     if (docRefTagsMap[node.tagName]) {
-      docRefTagsMap[node.tagName](newNode, node, documentRef);
+      docRefTagsMap[node.tagName](newNode, node, docParams);
     }
 
-    var childDepth = depth + 1,
-        childCount = newNode.childElementCount,
-        width = (end - start) / childCount,
-        child,
-        childStart;
+    var childDepth = depth + 1;
+    var childCount = newNode.childElementCount;
+    var width = (end - start) / childCount;
+    var child;
+    var childStart;
 
-    for (var i = 0; i< childCount; i++) {
-
+    for (i = 0; i < childCount; i++) {
       childStart = start + (i * width);
 
-      child = traverseDomLikeNode(node.children[i], newNode, childDepth, childStart, childStart + width, documentRef);
+      child = traverseDomNodes(node.children[i], newNode, childDepth,
+        childStart, childStart + width, docParams);
       newNode.children.push(child);
     }
 
     // Nodes have firstChild/lastChild properties too!
     if (childCount) {
       newNode.firstElementChild = newNode.children[0];
-      newNode.lastElementChild = newNode.children[childCount-1];
+      newNode.lastElementChild = newNode.children[childCount - 1];
     }
 
     // Binding relationship between sibling elements
     newNode.children.forEach(function(child, childIndex) {
       if (childIndex > 0) {
-        child.previousElementSibling = newNode.children[childIndex-1];
+        child.previousElementSibling = newNode.children[childIndex - 1];
       }
       if (childIndex < childCount - 1) {
-        child.nextElementSibling = newNode.children[childIndex+1];
+        child.nextElementSibling = newNode.children[childIndex + 1];
       }
     });
 
@@ -235,17 +231,16 @@ var domToCanvas = (function(domToCanvasOpts) {
    * Create a DOM-like structure. We will be using vanilla objects as our nodes, and making use of a few
    * DOM functions to traverse between each node.
    *
-   * @param myDocument {Document}
-   * @param start {Number} - canvas starting point
-   * @param end {Number} - canvas ending point
-   * @returns {Object}  Dom-like Tree
+   * @param {Document} myDoc - document we are transforming into a tree
+   * @param {Number} start - canvas starting point
+   * @param {Number} end - canvas ending point
+   * @return {Object} Dom-like Tree
    */
-  function createDOMLikeObject(myDocument, start, end) {
-
+  function createDOMLikeObject(myDoc, start, end) {
     /**
      * The document node, unlike other nodes, stores a reference to ids and certain types of nodes (images, scripts, etc).
      */
-    var documentRef = {
+    var docParams = {
 
       body: null, // reference to the <body> element
       head: null, // reference to the <head> element
@@ -261,42 +256,43 @@ var domToCanvas = (function(domToCanvasOpts) {
       largestDepth: 0
     };
 
-    var newDocument = traverseDomLikeNode(myDocument, null, 0, start, end, documentRef);
-    var augmentedDoc = Object.assign({}, newDocument, documentRef);
+    var newDocument = traverseDomNodes(myDoc, null, 0, start, end, docParams);
+    var augmentedDoc = Object.assign({}, newDocument, docParams);
     return augmentedDoc;
   }
 
   /**
    * Recursively travel down the dom-like tree, rendering each node as we go.
-   * @param ctx
-   * @param node
+   * @param {CanvasRenderingContext2D} ctx - ctx for rendering a canvas
+   * @param {Element} node - Node that we are drawing
+   * @param {Number} height - Height of the current node.
    */
   function drawNodes(ctx, node, height) {
+    var tagName = node.tagName;
+    var x = (node.start + (node.end - node.start) / 2);
+    var y = node.depth * height + 20;
 
-    var tagName = node.tagName,
-        x = (node.start + (node.end - node.start) / 2),
-        y = node.depth * height + 20;
-
-    var firstElementChild = node.firstElementChild,
-        lastElementChild = node.lastElementChild,
-        firstX,
-        firstY,
-        lastX,
-        lastY;
+    var firstChild = node.firstElementChild;
+    var lastChild = node.lastElementChild;
+    var firstX;
+    var firstY;
+    var lastX;
+    var lastY;
+    var fill;
 
     /**
      * Drawing the lines between sibling elements.
      * For us, that just means draw a line from the first to last child elements.
      */
-    if (firstElementChild && firstElementChild !== lastElementChild) {
-      firstX = (firstElementChild.start + (firstElementChild.end - firstElementChild.start) / 2);
-      firstY = firstElementChild.depth * height + 20;
+    if (firstChild && firstChild !== lastChild) {
+      firstX = (firstChild.start + (firstChild.end - firstChild.start) / 2);
+      firstY = firstChild.depth * height + 20;
 
-      lastX = (lastElementChild.start + (lastElementChild.end - lastElementChild.start) / 2);
-      lastY = lastElementChild.depth * height + 20;
+      lastX = (lastChild.start + (lastChild.end - lastChild.start) / 2);
+      lastY = lastChild.depth * height + 20;
 
       ctx.beginPath();
-      ctx.moveTo(firstX,firstY);
+      ctx.moveTo(firstX, firstY);
       ctx.lineTo(lastX, lastY);
       ctx.stroke();
       ctx.closePath();
@@ -310,13 +306,12 @@ var domToCanvas = (function(domToCanvasOpts) {
      * https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/children
      */
     node.children.forEach(function(child) {
-
       // Draw a line from our current node, to each of its children
-      var childX = (child.start + (child.end - child.start) / 2),
-          childY = child.depth * height + 20;
+      var childX = (child.start + (child.end - child.start) / 2);
+      var childY = child.depth * height + 20;
 
       ctx.beginPath();
-      ctx.moveTo(x,y);
+      ctx.moveTo(x, y);
       ctx.lineTo(childX, childY);
       ctx.stroke();
       ctx.closePath();
@@ -324,8 +319,9 @@ var domToCanvas = (function(domToCanvasOpts) {
       drawNodes(ctx, child, height);
     });
 
+    fill = nodeColorMap[tagName] ? nodeColorMap[tagName] : nodeColorMap.default;
     ctx.beginPath();
-    ctx.fillStyle = nodeColorMap[tagName] ? nodeColorMap[tagName] : nodeColorMap.default;
+    ctx.fillStyle = fill;
     ctx.arc(x, y, radius, startAngle, endAngle, false);
     ctx.fill();
 
@@ -342,21 +338,25 @@ var domToCanvas = (function(domToCanvasOpts) {
   /**
    * Recursively travel down the DOM tree, comparing the current node with the x=y coordinates of
    * the last click or hover event.
+   * @param {Element} node - Current element we are looking at
+   * @param {Number} x - x-coordinate
+   * @param {NUmber} y - y-coordinate
+   * @return {Node} node - node that is in our "click" region
    */
   function searchForNodeWithXY(node, x, y) {
-
-    var verticalCenter = (node.start + (node.end - node.start) / 2),
-        horizontalCenter = node.depth * cellHeight + 20,
-        isInNode = (x >= verticalCenter - radius && x <= verticalCenter + radius && y >= horizontalCenter - radius && y <= horizontalCenter + radius),
-        child,
-        i;
+    var vCenter = (node.start + (node.end - node.start) / 2);
+    var hCenter = node.depth * cellHeight + 20;
+    var isInX = x >= vCenter - radius && x <= vCenter + radius;
+    var isInY = y >= hCenter - radius && y <= hCenter + radius;
+    var isInNode = (isInX && isInY);
+    var child;
+    var i;
 
     if (isInNode) {
       return node;
     }
 
-    for (i = 0; i< node.childElementCount; i++) {
-
+    for (i = 0; i < node.childElementCount; i++) {
       child = node.children[i];
 
       /**
@@ -377,16 +377,15 @@ var domToCanvas = (function(domToCanvasOpts) {
    * Instead, we need to figure that ourselves. In our case, we traverse down the tree until we find the node that
    * we were trying to click on.
    *
-   * @param event {Event} - some browsers will include a global event, but its always safer to declare it yourself.
-   * For instance, last time I checked FireFox doesn't give you a freebe event object
+   * @param {MouseEvent} event - some browsers will include a global event, but its always safer to declare it yourself.
+   * For instance, last time I checked FireFox doesn't give you a freebee event object
    */
   function handleCanvasClick(event) {
-
-    var x = event.offsetX,
-        y = event.offsetY,
-        canvas = ctx.canvas,
-        found,
-        domLike;
+    var x = event.offsetX;
+    var y = event.offsetY;
+    var canvas = ctx.canvas;
+    var found;
+    var domLike;
 
     /**
      * We're using a stack (actually just an array we are treating like a stack)
@@ -432,16 +431,15 @@ var domToCanvas = (function(domToCanvasOpts) {
   /**
    * If the user hovers over a node on the canvas, we want to reflect what they are hovering over
    * by "highlighting" that node on the current document.
-   * @param event
+   * @param {MouseEvent} event - Mouse move event
    */
   function handleCurrentDocumentMouseMove(event) {
-
-    var x = event.offsetX,
-        y = event.offsetY,
-        foundNode = searchForNodeWithXY(currentTree, x, y),
-        nodeText,
-        foundX,
-        foundY;
+    var x = event.offsetX;
+    var y = event.offsetY;
+    var foundNode = searchForNodeWithXY(currentTree, x, y);
+    var nodeText;
+    var foundX;
+    var foundY;
 
     if (!foundNode) {
       return;
@@ -454,40 +452,37 @@ var domToCanvas = (function(domToCanvasOpts) {
      * For now, the text is of the form:  TAGNAME#id
      */
     if (foundNode.tagName) {
-
       createDOMLikeObject(currentTree.__nodeRef, 0, ctx.canvas.width);
       drawDOM(ctx.canvas, currentTree);
       nodeText = [
         foundNode.tagName,
-        (foundNode.id ? '#'+foundNode.id : '')
+        (foundNode.id ? '#' + foundNode.id : '')
       ].join('');
 
       ctx.fillStyle = '#000';
-      foundX = foundNode.start + (foundNode.end - foundNode.start) /2;
+      foundX = foundNode.start + (foundNode.end - foundNode.start) / 2;
       foundY = foundNode.depth * currentTreeHeight + 20;
       ctx.fillText(nodeText, foundX + 5, foundY - 5);
     }
 
-
-    var domNode = foundNode.__nodeRef,
-        domNodeStyle = domNode.style;
+    var domNode = foundNode.__nodeRef;
+    var domNodeStyle = domNode.style;
 
     if (currentHoveredNode) {
-      currentHoveredNode.style.backgroundColor = currentHoveredNodeBackgroundColor;
+      currentHoveredNode.style.backgroundColor = previousNodeBackgroundColor;
     }
 
     if (domNodeStyle) {
-      currentHoveredNodeBackgroundColor = domNodeStyle.backgroundColor;
+      previousNodeBackgroundColor = domNodeStyle.backgroundColor;
       domNodeStyle.backgroundColor = 'rgba(255, 255,0, 0.4)';
       currentHoveredNode = domNode;
     }
-
   }
 
   /**
    * Given a canvas and a HTMLDocument, render nodes onto our canvas
-   * @param canvas {Element}
-   * @param myDocument {Document}
+   * @param {Element} canvas - Canvas that we are rendering a tree onto
+   * @param {Document} myDocument - Document that we want to render into a tree
    */
   function drawDOM(canvas, myDocument) {
     if (!myDocument instanceof HTMLDocument) {
@@ -502,7 +497,6 @@ var domToCanvas = (function(domToCanvasOpts) {
      * https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
      */
     ctx = canvas.getContext('2d');
-
 
     var domLike = createDOMLikeObject(myDocument, 0, canvas.width);
     currentTree = domLike;
@@ -545,14 +539,12 @@ var domToCanvas = (function(domToCanvasOpts) {
     });
   }
 
-
   /**
    * Render the current page's document tree onto a canvas.
-   * @param width
-   * @param height
+   * @param {Number} width - size of canvas
+   * @param {Number} height - size of canvas
    */
   function renderCurrentDOM(width, height) {
-
     /**
      * If there is no global document or document.createElement, this function is going to crash and burn.
      * So I am adding a safety check and exiting early if that happens.
@@ -573,7 +565,6 @@ var domToCanvas = (function(domToCanvasOpts) {
      */
     var documentFragment = document.createDocumentFragment();
 
-
     /**
      * To create dom element with text in it, we have to first create the dom element, then we need to create
      * a "text node", and append that text node to the dom element.
@@ -589,7 +580,7 @@ var domToCanvas = (function(domToCanvasOpts) {
      *
      * But, if the number of strings being concat is small, then it makes more sense to "+" the strings together.
      */
-    var cssText = [
+    var canvasCSSText = [
       'background: rgba(255,255,255,0.8)',
       'border: 1px solid #ccc',
       'cursor: pointer',
@@ -599,6 +590,14 @@ var domToCanvas = (function(domToCanvasOpts) {
       'z-index: 999999'
     ].join(';');
 
+    var closeDivCSSText = [
+      'position:fixed',
+      'right: 10px',
+      'top: 10px',
+      'z-index:9999999',
+      'cursor:pointer'
+    ].join(';');
+
     /**
      * There are multiple ways to style this element.
      *
@@ -606,10 +605,8 @@ var domToCanvas = (function(domToCanvasOpts) {
      * style.cssText -  allows you to add a series of styles in a single string
      * style.background - you can set individual styles directly.
      */
-    canvas.style.cssText = cssText;
-
-    closeDiv.style.cssText = 'position:fixed;right: 10px;top: 10px;z-index:9999999;cursor:pointer;';
-
+    canvas.style.cssText = canvasCSSText;
+    closeDiv.style.cssText = closeDivCSSText;
 
     /**
      * Even if you set the css height and width of the canvas, what actually gets rendered will look
@@ -652,7 +649,7 @@ var domToCanvas = (function(domToCanvasOpts) {
 
       // If we did have a "hovered" style, revert it back to its original background
       if (currentHoveredNode) {
-        currentHoveredNode.style.backgroundColor = currentHoveredNodeBackgroundColor;
+        currentHoveredNode.style.backgroundColor = previousNodeBackgroundColor;
       }
     });
 
@@ -665,7 +662,6 @@ var domToCanvas = (function(domToCanvasOpts) {
      */
 
     var observer = new MutationObserver(function() {
-
       /**
        * Update all the previous instances of the treestack, rebuilding out DOMLikeObjects with new dimentions to
        * account for the added/removed nodes.
